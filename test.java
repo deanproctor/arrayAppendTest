511392638858383825ead6a6a0bafba7599d9376
root <root@ip-10-0-19-96.us-west-2.compute.internal>
2016-11-04 20:19:56 +0000

    added response logging

--- test.java
+++ test.java
@@ -10,18 +10,22 @@ public class test {
     	Cluster cluster = CouchbaseCluster.create("ec2-35-160-22-211.us-west-2.compute.amazonaws.com");
     	Bucket bucket = cluster.openBucket("test");
 
-        Observable
+	Observable
                 .just(0)
                 .flatMap(s ->
                         Observable.range(0, 1000)
                                 .flatMap(new Func1<Integer, Observable<?>>() {
                                         @Override
                                         public Observable<?> call(Integer i) {
-                                                return bucket.async().mutateIn(s.toString()).arrayAppend("recs", 1, false).execute();
-                                        }
-                                }))
+                                                return bucket.async().mutateIn(s.toString()).arrayAppend("recs", 1, false).execute()
+							.map(result -> {
+								System.out.println(result.status(0).toString() + "\n");
+								return this;
+							});
+                                		}}))
                 .toList().toBlocking().single();
 
+	bucket.close();
 	cluster.disconnect();
     }
 }
